475a546b5c0ef8629cfb8ad770994c075d748b1f
faizathaya19 <faizathayaramadhan@gmail.com>
2023-05-30 17:07:25 +0700

    add spp screen and pocket money screen

--- lib/ui/Screens/spppaymentrec_screen.dart
+++ lib/ui/Screens/spppaymentrec_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../constants/const.dart';
+import '../widgets/spppaymentrec_widget.dart';
 import 'home_screen.dart';
 
 class SPPPaymentrecScreen extends StatefulWidget {
@@ -15,65 +16,49 @@ class _SPPPaymentrecScreenState extends State<SPPPaymentrecScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: backgroundColor1,
-          elevation: 0,
-          toolbarHeight: 100, // Atur tinggi khusus untuk toolbar
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back_ios_new_outlined),
-            onPressed: () {
-              Navigator.popAndPushNamed(context, HomeScreen.id);
-            },
+      appBar: AppBar(
+        backgroundColor: backgroundColor1,
+        elevation: 0,
+        toolbarHeight: 100, // Atur tinggi khusus untuk toolbar
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new_outlined),
+          onPressed: () {
+            Navigator.popAndPushNamed(context, HomeScreen.id);
+          },
+        ),
+        title: const Text(
+          'Rekap Pembayaran SPP',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 15,
           ),
-          title: const Text(
-            'Rekap Pembayaran SPP',
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              fontSize: 15,
+        ),
+        centerTitle: true,
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          InkWell(
+            onTap: () {},
+            child: CustomCard(
+              textharga: 'Rp 4,900,000',
+              texttanggal: 'Oktober 2022',
+              textstatus: 'Lunas',
             ),
           ),
-          centerTitle: true,
-        ),
-        body: Stack(
-          children: [
-            SafeArea(
-                child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  Expanded(
-                      child: GridView.count(
-                    crossAxisCount: 2,
-                    children: [
-                      Card(
-                        elevation: 4,
-                        child: Column(
-                          children: [
-                            Text('Rp 4,900,000'),
-                            Text(
-                              'Oktober 2022',
-                            ),
-                            Expanded(
-                              child: Align(
-                                alignment: Alignment.bottomCenter,
-                                child: Container(
-                                  height: 50,
-                                  width: 250,
-                                  color: Colors.green,
-                                  child: Text('Lunas'),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ))
-                ],
-              ),
-            ))
-          ],
-        ));
+          InkWell(
+            onTap: () {
+              Navigator.pushReplacementNamed(context, SPPPaymentrecScreen.id);
+            },
+            child: CustomCard(
+              textharga: 'Rp 4,900,000',
+              texttanggal: 'Oktober 2022',
+              textstatus: 'Belum Lunas',
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
